a5f00b303d820486adc1f1298c27fd0a66042976
IHenor <henor2003@gmail.com>
2023-03-26 23:49:49 +0200

    disa ndryshime ne signUp

--- client/src/components/signUpCard.js
+++ client/src/components/signUpCard.js
@@ -1,26 +1,27 @@
 import React from "react";
 import "../css/signUp.css";
+import Login from "..//components/Login"
 
 const SignUpCard = () => {
   return (
     <>
+  
     <section class="body">
       <div class="diamond"></div>
-    <div class="card-left">
-      <div class="card-main-left">
+      <div class="card-left">
+    </div>
+    <div class="card-mid">
+      <div class="card-main-mid">
         <h1>Create a new account now</h1>
-  <input type="text" id="name" name="name" placeholder="Name" class="input-info"/>
-  <input type="text" id="email" name="email" placeholder="Email" class="input-info"/>
-  <input type="password" id="password" name="pas" placeholder="Password" class="input-info"/>
+ {/*<input type="text" id="name" name="name" placeholder="Name" class="input-info"/>*/}
+      <input type="text" id="email" name="email" placeholder="Email" class="input-info"/>
+      <input type="password" id="password" name="pas" placeholder="Password" class="input-info"/>
+      <p>Already have an account? <u>Log In</u></p>
         <button class="input-info" id="btn-id"> Confirm </button>
-        
       </div>
     </div>
     <div class="card-right">
-    <div class="card-main-right">
-        <h1>Create a new account now</h1>
-        
-      </div>
+    
     </div>
     </section>    
     </>
