d3f980db476f6bd532f886c67354df22f449d186
IHenor <henor2003@gmail.com>
2023-04-02 00:18:07 +0200

    lmao

--- client/src/components/signUpCard.js
+++ client/src/components/signUpCard.js
@@ -1,28 +1,49 @@
-import React from "react";
+import React,{useRef,useEffect,useState} from "react";
 import "../css/signUp.css";
-import Login from "..//components/Login"
+
 
 const SignUpCard = () => {
+
+  const userRef=useRef();
+  const errRef = useRef();
+
+  const [user,setUser] = useState('')
+  const [pass,setPass] = useState('')
+  const [errMsg,setErrMsg] = useState('')
+  const [succes,setSucces] = useState(false)
+
+  useEffect(()=>{
+    setErrMsg('')
+  },[user,pass])
+
+  const handleSubmit = async e =>{
+    e.preventDefault()
+    console.log(user,pass)
+    setUser('')
+    setPass('')
+    setSucces(true)
+  }
+
+
   return (
     <>
   
     <section class="body">
-      <div class="diamond"></div>
-      <div class="card-left">
-    </div>
-    <div class="card-mid">
-      <div class="card-main-mid">
-        <h1>Create a new account now</h1>
- {/*<input type="text" id="name" name="name" placeholder="Name" class="input-info"/>*/}
-      <input type="text" id="email" name="email" placeholder="Email" class="input-info"/>
-      <input type="password" id="password" name="pas" placeholder="Password" class="input-info"/>
-      <p>Already have an account? <u>Log In</u></p>
-        <button class="input-info" id="btn-id"> Confirm </button>
-      </div>
-    </div>
-    <div class="card-right">
     
+    <div class="card-mid">
+      <form class="card-main-mid" onSubmit={handleSubmit}>
+        <h1>Create a new account now</h1>
+ {/*<p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>*/}
+      
+      <input type="text" id="email" ref={userRef} autoComplete="off" onChange={e => setUser(e.target.value)} value={user} placeholder="Email" class="input-info" required/>
+      <input type="password" id="password" autoComplete="off" onChange={e => setPass(e.target.value)} value={pass} placeholder="Password" class="input-info" required/>
+      
+      <p>Already have an account? <a href="#"><u>Log In</u></a></p>
+      <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+        <button class="input-info" id="btn-id"> Confirm </button>
+      </form>
     </div>
+   
     </section>    
     </>
   );
