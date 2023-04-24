7dd87c6fd5ca7b35f64488733d68b71ed5468e16
IHenor <henor2003@gmail.com>
2023-04-24 17:02:53 +0200

    Training page commit

--- client/src/pages/Training.js
+++ client/src/pages/Training.js
@@ -1,7 +1,24 @@
 import React from "react";
+import "../css/training.css";
+import Item_training_wrapper from "../components/Item_training_wrapper";
 
 const Training = () => {
-  return <div>Training</div>;
+  return (<>
+    <div className="main_banner_training">
+        <div className="main_banner_training_content">
+         <h1>Lorem ipsum dolor sit amet, consectetur adipisicing.</h1>
+         <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+      </div>
+    </div>
+    <section className="training_section">
+    <Item_training_wrapper/>
+    
+    </section>
+    
+
+
+
+  </>)
 };
 
 export default Training;
